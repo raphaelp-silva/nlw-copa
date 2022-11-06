cd118fe251650146ba7a7ca8f052327c0ed07e19
Raphaelp-silva <raphaelp.silva@outlook.com>
2022-11-06 11:31:11 -0300

    adding another matchs

--- main.js
+++ main.js
@@ -30,11 +30,52 @@ document.querySelector("#app").innerHTML = `
                 <img src="./assets/logo.svg" alt="logo da nlw">
     </header>
     <main id="cards">
+    ${createCard("20/11", "domingo", 
+    createGame("qatar", "13:00", "ecuador"))}
+
+
+    ${createCard("21/11", "segunda", 
+     createGame("england", "10:00", "iran")+ 
+     createGame("senegal", "13:00", "netherlands")+ 
+     createGame("usa", "16:00", "wales"))}
+
+
+     ${createCard("22/11", "terça", 
+     createGame("argentina", "07:00", "saudiarabia")+ 
+     createGame("denmark", "10:00", "tunisia")+ 
+     createGame("mexico", "13:00", "poland")+ 
+     createGame("france", "16:00", "australia"))}
+
+     ${createCard("23/11", "quarta", 
+     createGame("morocco", "07:00", "croatia")+ 
+     createGame("germany", "10:00", "japan")+ 
+     createGame("spain", "13:00", "costarica")+ 
+     createGame("belgium", "16:00", "canada"))}
+
+  
      ${createCard("24/11", "quinta", 
      createGame("switzerland", "07:00", "cameroon")+ 
      createGame("uruguay", "10:00", "southkorea")+ 
      createGame("portugal", "13:00", "ghana")+ 
      createGame("brazil", "16:00", "serbia"))}
+
+     ${createCard("25/11", "sexta", 
+     createGame("wales", "07:00", "iran")+ 
+     createGame("qatar", "10:00", "senegal")+ 
+     createGame("netherlands", "13:00", "ecuador")+ 
+     createGame("england", "16:00", "usa"))}
+
+     ${createCard("26/11", "sábado", 
+     createGame("tunisia", "07:00", "australia")+ 
+     createGame("poland", "10:00", "saudiarabia")+ 
+     createGame("france", "13:00", "denmark")+ 
+     createGame("argentina", "16:00", "mexico"))}
+
+     ${createCard("27/11", "domingo", 
+     createGame("japan", "07:00", "costarica")+ 
+     createGame("belgium", "10:00", "morocco")+ 
+     createGame("croatia", "13:00", "canada")+ 
+     createGame("spain", "16:00", "germany"))}
      
      
      ${createCard("28/11", "segunda", 
@@ -42,6 +83,24 @@ document.querySelector("#app").innerHTML = `
      createGame("southkorea", "10:00", "ghana")+
      createGame("brazil", "13:00", "switzerland")+
      createGame("portugal", "16:00", "uruguay"))}
+
+     ${createCard("29/11", "terça", 
+     createGame("ecuador", "12:00", "senegal")+
+     createGame("netherlands", "12:00", "qatar")+
+     createGame("iran", "16:00", "usa")+
+     createGame("wales", "16:00", "england"))}
+
+     ${createCard("30/11", "quarta", 
+     createGame("tunisia", "12:00", "france")+
+     createGame("australia", "12:00", "denmark")+
+     createGame("poland", "16:00", "argentina")+
+     createGame("saudiarabia", "16:00", "mexico"))}
+
+     ${createCard("01/12", "quinta", 
+     createGame("croatia", "12:00", "belgium")+
+     createGame("canada", "12:00", "morocco")+
+     createGame("japan", "16:00", "spain")+
+     createGame("costarica", "16:00", "germany"))}
      
      
      ${createCard("02/12", "sexta",
